06c5d825a784d364501c23a15de5dfc259ff3c09
Meral Ocak <ocakmeral@gmail.com>
2020-06-16 21:32:20 +0300

    First commit

---
+++ model/model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 14:55:43 2020
+
+@author: meralocak
+"""
+
+
+# Import dependencies
+import pandas as pd
+import numpy as np
+
+# Load the dataset in a dataframe object and include only four features as mentioned
+url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+df = pd.read_csv(url)
+include = ['Age', 'Survived'] # Only four features
+df_ = df[include]
+
+# Data Preprocessing
+categoricals = []
+for col, col_type in df_.dtypes.iteritems():
+     if col_type == 'O':
+          categoricals.append(col)
+     else:
+          df_[col].fillna(0, inplace=True)
+
+df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+
+# Logistic Regression classifier
+from sklearn.linear_model import LogisticRegression
+dependent_variable = 'Survived'
+x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+y = df_ohe[dependent_variable]
+lr = LogisticRegression()
+lr.fit(x, y)
+
+# Save your model
+from sklearn.externals import joblib
+joblib.dump(lr, 'lr_model.pkl')
+print("Model dumped!")
+
+
+
