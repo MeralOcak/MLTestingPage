319e8932bfab40f5660503664aa0ff1a7f2b40b4
Meral Ocak <ocakmeral@gmail.com>
2020-06-17 11:29:19 +0300

    css file added

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ Created on Tue Jun 16 20:26:59 2020
 """
 
 
-import flask
 import joblib
-import pandas as pd
+import flask
+from flask import request, jsonify, render_template
+import numpy as np
 
 # Use pickle to load in the pre-trained model
 with open('lr_model.pkl', 'rb') as f:
@@ -18,30 +19,59 @@ with open('lr_model.pkl', 'rb') as f:
 # Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
 
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    return render_template('main.html', prediction_text='Prediction is $ {}'.format(prediction))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+
 # Set up the main route
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'GET':
-        # Just render the initial form, to get input
-        return(flask.render_template('main.html'))
+# @app.route('/', methods=['GET', 'POST'])
+# def main():
+#     if flask.request.method == 'GET':
+#         # Just render the initial form, to get input
+#         return(flask.render_template('main.html'))
     
-    if flask.request.method == 'POST':
-        # Extract the input
-        Age = flask.request.form['Age']
+#     if flask.request.method == 'POST':
+#         # Extract the input
+#         Age = flask.request.form['Age']
     
 
-        # Make DataFrame for model
-        input_variables = pd.DataFrame([Age],columns=['Age'],dtype=float, index=['input'])
+#         # Make DataFrame for model
+#         input_variables = pd.DataFrame([Age],columns=['Age'],dtype=float, index=['input'])
 
-        # Get the model's prediction
-        prediction = model.predict(input_variables)[0]
+#         # Get the model's prediction
+#         prediction = model.predict(input_variables)[0]
     
-        # Render the form again, but add in the prediction and remind user
-        # of the values they input before
-        return flask.render_template('main.html',
-                                     original_input={'Age':Age},
-                                     result=prediction,
-                                     )
+#         # Render the form again, but add in the prediction and remind user
+#         # of the values they input before
+#         return flask.render_template('main.html',
+#                                      original_input={'Age':Age},
+#                                      result=prediction,
+#                                      )
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug =True)
+    
+    
+
+
